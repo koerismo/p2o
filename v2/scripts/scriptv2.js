a15c5a3c3f6ac4543a4a77d92dbc53cfabd9ee2c
MeepJJ <jadon.lemkin@gmail.com>
2020-03-30 15:07:27 -0400

    made somewhat functional

--- v2/scripts/scriptv2.js
+++ v2/scripts/scriptv2.js
@@ -8,17 +8,28 @@ model_loader,
 mouse,
 pmouse,
 mouseBeingHeld = 0,
-holding_object = 0
+holding_object = 0,
+mouse_object,
+keys = {},
+items,
+gridSize = 64,
+selected;
 
 var euler = new THREE.Euler( 0, 0, 0, 'YXZ' );
 var PI_2 = Math.PI / 2;
 var vec = new THREE.Vector3();
 
+//code modified from PointerLockControls.js
 function onDocumentMouseMove(event) {
+  if (mouseBeingHeld && holding_object) {
+    let mp = getMousePos()
+    if (mp)
+      moveObjectToCursor(mouse_object[0].object,mp)
+  }
+  mouse.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
   if (mouseBeingHeld && !holding_object) {
     var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
     var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-    console.log(movementX,movementY)
     euler.setFromQuaternion( camera.quaternion );
     euler.y -= movementX * 0.002;
     euler.x -= movementY * 0.002;
@@ -27,34 +38,64 @@ function onDocumentMouseMove(event) {
   }
 }
 function onDocumentMouseDown(event) {
-  let mouse_object = getSelectedObjects()
-  holding_object = (mouse_object.length > 0)
+  if (!mouseBeingHeld) {
+    mouse_object = getSelectedObjects()
+    //if (selected != undefined)
+      //selected.object.material.emissive.setHex(0x0);
+    selected = mouse_object[0]
+    //if (mouse_object.length)
+      //selected.object.material.emissive.setHex(0xfff);
+  }
   mouseBeingHeld = 1;
-  if (mouse_object.length)
-    mouse_object = mouse_object[0]
-  else
+  if ((mouse_object.length && mouse_object[0].object.name == "item")){
+      //begin holding object
+      holding_object = 1;
+  }
+  else {
     pointer_lock()
-    return;
+
+  }
 }
+
 function onDocumentMouseUp(event) {
   pointer_unlock()
   mouseBeingHeld = 0
   holding_object = 0
 }
 function onDocumentKeyDown(event) {
-  //camera.position.addScalar((0,50,0));
-  if (event.keyCode == 87) {
-    moveForward(1)
+  if (event.keyCode == 189) {
+    scene.remove(selected.object)
   }
+  if (event.keyCode == 187) {
+    addVoxelFromSelection(selected)
+  }
+  if (event.keyCode == 219) {
+    gridSize *= 0.5
+    cubeGeo = new THREE.BoxGeometry( gridSize, gridSize, gridSize );
+  }
+  if (event.keyCode == 221) {
+    gridSize *= 2
+    cubeGeo = new THREE.BoxGeometry( gridSize, gridSize, gridSize );
+  }
+  keys[event.keyCode] = true;
 
 }
+function onDocumentKeyPress(event) {
+}
 function onDocumentKeyUp(event) {
-
+  keys[event.keyCode] = false;
 }
 
 function getSelectedObjects() {
   raycaster.setFromCamera( mouse, camera );
   return raycaster.intersectObjects( scene.children );
+}
+
+function getMousePos() {
+  raycaster.setFromCamera( mouse, camera );
+  let intlist = raycaster.intersectObjects( scene.children.filter(function(x){return x.name != "item"}) )
+  if (intlist.length)
+    return intlist[0].point.divideScalar(gridSize).floor().multiplyScalar(gridSize).addScalar(gridSize/2);
 }
 
 function render() {
@@ -64,6 +105,18 @@ function render() {
 }
 
 function animate() {
+  if (keys[87]) {
+    moveForward(5)
+  }
+  if (keys[83]) {
+    moveForward(-5)
+  }
+  if (keys[68]) {
+    moveRight(5)
+  }
+  if (keys[65]) {
+    moveRight(-5)
+  }
   requestAnimationFrame( animate );
   //controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
   render();
@@ -76,8 +129,8 @@ function init() {
 
   raycaster = new THREE.Raycaster()
 
-  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 )
-  camera.position.set( -50, -50, 0 );
+  camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 1, 4000 )
+  camera.position.set( 0, -1, 0 );
 
   scene = new THREE.Scene()
   scene.background = new THREE.Color( 0xcccccc );
@@ -90,21 +143,49 @@ function init() {
   scene.add( light );
   var light = new THREE.AmbientLight( 0x222222 );
   scene.add( light );
-  //var voxel = new THREE.Mesh( cubeGeo, material );
-  //scene.add( voxel );
-  var axesHelper = new THREE.AxesHelper( 10 );
-  scene.add( axesHelper );
-  var gridHelper = new THREE.GridHelper( 10, 10 );
-  scene.add( gridHelper );
+  //var axesHelper = new THREE.AxesHelper( 10 );
+  //scene.add( axesHelper );
+  //var gridHelper = new THREE.GridHelper( 10, 10 );
+  //scene.add( gridHelper );
+
+  mat_loader = new THREE.TextureLoader()
+  diffuseTex = mat_loader.load( 'https://cdn.glitch.com/5c1db9b3-25f5-4e14-9f6f-7ab10f4855d0%2Fnormal_0.png?v=1584673408782');
+
+  //model_loader = new THREE.OBJLoader()
+
+  var geometry = new THREE.SphereBufferGeometry( 32 );
+  material = new THREE.MeshPhysicalMaterial( {map: diffuseTex} );
+  cubeGeo = new THREE.BoxGeometry( gridSize, gridSize, gridSize);
+
+  for ( var z = -5; z <= 5; z ++ ) {
+    for ( var y = -5; y <= 5; y ++ ) {
+      for ( var x = -5; x <= 5; x ++ ) {
+        if (Math.abs(x) > 4 || Math.abs(y) > 4 || Math.abs(z) > 4)
+        addTile(x,y,z,cubeGeo,material)
+      }
+    }
+  }
+
+  var voxel = new THREE.Mesh( geometry, material );
+  voxel.name = "item"
+  scene.add( voxel );
+  var light = new THREE.PointLight( "#fff", 1, 500 );
+  voxel.castShadow = false;
+  light.castShadow = true;
+  voxel.add(light)
+  light.castShadow = true;
+	light.shadow.camera.near = 8;
+	light.shadow.camera.far = 30;
+	light.shadow.mapSize.width = 1024;
+	light.shadow.mapSize.height = 1024;
+  //light.position.set( 50, 50, 50 );
 
   renderer = new THREE.WebGLRenderer( { antialias: true } )
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.BasicShadowMap;
   renderer.setPixelRatio( window.devicePixelRatio )
   renderer.setSize( window.innerWidth, window.innerHeight )
   document.body.appendChild( renderer.domElement )
-
-  mat_loader = new THREE.TextureLoader()
-
-  //model_loader = new THREE.OBJLoader()
 
   mouse = new THREE.Vector2(0,0)
   pmouse = new THREE.Vector2(0,0)
@@ -112,20 +193,22 @@ function init() {
   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
   document.addEventListener( 'mouseup', onDocumentMouseUp, false );
   document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+  //document.addEventListener( 'click', onDocumentMouseClick, false );
   document.addEventListener( 'keydown', onDocumentKeyDown, false );
   document.addEventListener( 'keyup', onDocumentKeyUp, false );
 }
 
+//code modified from PointerLockControls.js to allow vertical flying
 function moveForward( distance ) {
-  vec.setFromMatrixColumn( camera.matrix, 0 );
-  vec.crossVectors( camera.up, vec );
-  camera.position.addScaledVector( vec, distance );
+  vec.setFromMatrixColumn( camera.matrix, 2 );
+  camera.position.addScaledVector( vec, distance*-1 );
 
 };
 
 function moveRight( distance ) {
-  vec.setFromMatrixColumn( camera.matrix, 0 );
-  camera.position.addScaledVector( vec, distance/500 );
+  vec.setFromMatrixColumn( camera.matrix, 2 );
+  vec.cross( camera.up )
+  camera.position.addScaledVector( vec, distance*-1 );
 };
 
 function pointer_lock() {
@@ -135,6 +218,32 @@ function pointer_lock() {
 function pointer_unlock() {
   document.exitPointerLock();
 };
+
+function addTile(x,y,z,g,m) {
+  var wall = new THREE.Mesh( g, m );
+  wall.position.x = x*gridSize+gridSize/2;
+  wall.position.y = y*gridSize+gridSize/2;
+  wall.position.z = z*gridSize+gridSize/2;
+  wall.updateMatrix();
+  wall.matrixAutoUpdate = false;
+  scene.add( wall );
+}
+
+function addVoxelFromSelection(intersect) {
+  var voxel = new THREE.Mesh( cubeGeo, material );
+  voxel.position.copy( intersect.point ).add( intersect.face.normal );
+  voxel.position.divideScalar( gridSize ).floor().multiplyScalar( gridSize ).addScalar( gridSize/2 );
+  voxel.castShadow = true;
+  voxel.receiveShadow = true;
+  voxel.matrixAutoUpdate  = false;
+  voxel.updateMatrix();
+  scene.add( voxel );
+}
+
+function moveObjectToCursor(obj,pos) {
+  obj.position.copy( pos )
+  //obj.position.divideScalar( gridSize ).floor().multiplyScalar( gridSize ).addScalar( gridSize/2 );
+}
 
 init()
 animate()
