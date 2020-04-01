005ee680e37aacbc5fe49cf4a8208d6f31718086
koerismo <jadonl23@parkschool.net>
2020-04-01 19:12:45 -0400

    did things

--- v2/scripts/loader.js
+++ v2/scripts/loader.js
@@ -1,3 +1,5 @@
+var packages = {};
+
 function loadFile(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.ontimeout = function () {
@@ -15,42 +17,22 @@ function loadFile(url, callback) {
     xhr.open("GET", url, true);
     xhr.send(null);
 }
-function req(url,header,func) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.callback = func
-  xhttp.open("GET", url, true);
-  if (header)
-    xhttp.setRequestHeader("data",JSON.stringify(header))
-  xhttp.send();
-}
 
 var itembar = document.getElementById("sidebar_left")
 console.log("retrieving packages...")
 loadFile("packages.json",function(x){ //this file does not exist. When requested, the server should return a JSON formatted list of folders inside the packages folder
-  if (x.status) {
-    JSON.parse(x.responseText).forEach(function(y){
-
-      console.log("downloading "+y+"...")
-      loadFile("packages/"+y+"/package.json",function(z){
-        if (z.status) {
-          console.log("downloaded "+y+" information.")
-          var itemgroup = document.createElement("DIV")
-          z.items.forEach(function(a){
-            let im = document.createElement("IMG")
-            im.src = "packages/"+y+"/editor/"+a.icon
-            itemgroup.appendChild(im)
-          })
-          itembar.appendChild(itemgroup)
-        }
-        else {
-          console.log("failed to download "+y+".")
-        }
-      })
+  var pkglist = JSON.parse(x.response)
+  console.log("found "+pkglist.length+" packages. Downloading...")
+  pkglist.forEach(function(y,i){
+    loadPackage(y,function(z){
+      console.log("downloaded package "+i+"/"+pkglist.length)
+    })
+  })
     })
   }
 })
 
-function loadPackage(pkgname) {
+function loadPackage(pkgname,callback) {
   loadFile("/packages/"+pkgname+"/package.json",function(x){
 
     var itemgroup = document.createElement("DIV")
