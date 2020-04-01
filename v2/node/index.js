4235f7068b25f18fdf53a63f2aaaada5971716e5
koerismo <jadonl23@parkschool.net>
2020-04-01 17:42:44 -0400

    began package loader

--- v2/node/index.js
+++ v2/node/index.js
@@ -4,12 +4,13 @@ var fs = require('fs');
 //create a server object:
 http.createServer(function (req, res) {
   if (req.url == "/packages.json") {
-      let di = fs.readdir("../packages",function(a,x){
-        res.write(JSON.stringify({list:x}))
+      let di = fs.readdir(__dirname+"/../packages",function(a,x){
+        res.write(JSON.stringify(x))
         res.end();
-      }
+      })
+    }
+  else {
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
   }
-
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
